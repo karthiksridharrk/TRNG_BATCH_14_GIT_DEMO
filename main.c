2981b6b02fca785f4f77bfbfded0d380d43ec2b4
karthiksridharrk <47099817+karthiksridharrk@users.noreply.github.com>
2019-01-28 18:56:49 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
-#include<conio.h>
-int add(int a,int b);
-int sub(int a,int b);
-int mul(int a,int b);
-int div(int a,int b);
-int mod(int a,int b);
-
-int main(int a,int b)
+#include"div.h"
+#include"add.h"
+#include"mul.h"
+#include"mod.h"
+#include"sub.h"
+int main()
 {
-int a, b, c;
-clrscr();
+ int a, b, c;
 printf("Enter two numbers for arithmetic operation\n");
 scanf("%d%d",&a,&b);
-add(int a,int b);
-sub(int a,int b);
-mul(int a,int b);
-div(int a,int b);
-mod(int a,int b);
-getch();
+add(a,b);
+sub(a,b);
+mul(a,b);
+div(a,b);
+mod(a,b);
+return 0;
 }
